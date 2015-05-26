105081e729c3d9f7a9719f481f72216ebe00af3f
mootensai <mootensai@users.noreply.github.com>
2015-05-26 09:12:19 +0700

    Update namespace

--- RelationTrait.php
+++ RelationTrait.php
@@ -5,7 +5,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-namespace mootensai\relation\traits;
+namespace mootensai\relation;
 trait RelationTrait{
     
     public function loadWithRelation($POST) {
