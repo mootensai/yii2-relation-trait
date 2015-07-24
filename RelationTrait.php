87d5ed0b0f4d8e80240949a316a17a35d5fd1089
Yohanes Candrajaya <mootensai@users.noreply.github.com>
2015-07-24 14:14:34 +0700

    Update RelationTrait.php
    Add deleteAll()
    Fix saveAll() with relatedErrors

--- RelationTrait.php
+++ RelationTrait.php
@@ -6,6 +6,7 @@
  * @author Yohanes Candrajaya <moo.tensai@gmail.com>
  * @since 1.0
  */
+
 namespace mootensai\relation;
 
 use \yii\db\ActiveRecord;
@@ -13,8 +14,8 @@ use \yii\db\Exception;
 use \yii\helpers\Inflector;
 use \yii\helpers\StringHelper;
 
-trait RelationTrait{
-    
+trait RelationTrait {
+
     public function loadAll($POST) {
         if ($this->load($POST)) {
             $shortName = StringHelper::basename(get_class($this));
@@ -46,7 +47,7 @@ trait RelationTrait{
             return false;
         }
     }
-    
+
     public function saveAll() {
         /* @var $this ActiveRecord */
         $db = $this->getDb();
@@ -61,47 +62,47 @@ trait RelationTrait{
                     $relPKAttr = $records[0]->primaryKey();
                     $isCompositePK = (count($relPKAttr) > 1);
                     /* @var $relModel ActiveRecord */
-                    foreach($records as $index => $relModel){
-                        foreach ($link as $key => $value){
+                    foreach ($records as $index => $relModel) {
+                        foreach ($link as $key => $value) {
                             $relModel->$key = $this->$value;
                             $notDeletedFK[$key] = "$key = '{$this->$value}'";
                         }
                         $relSave = $relModel->save();
-                        if(!$relSave){
+                        if (!$relSave || !empty($relModel->errors)) {
                             $relModelWords = Inflector::camel2words(StringHelper::basename($AQ->modelClass));
                             $index++;
-                            foreach ($relModel->errors as $validation){
-                                foreach($validation as $errorMsg){
-                                    $this->addError($name,"$relModelWords #$index : $errorMsg");
+                            foreach ($relModel->errors as $validation) {
+                                foreach ($validation as $errorMsg) {
+                                    $this->addError($name, "$relModelWords #$index : $errorMsg");
                                 }
                             }
                             $error = 1;
-                        }else{
+                        } else {
                             //GET PK OF REL MODEL
-                            if($isCompositePK){
-                                foreach($relModel->primaryKey as $attr => $value){
+                            if ($isCompositePK) {
+                                foreach ($relModel->primaryKey as $attr => $value) {
                                     $notDeletedPK[$attr][] = "'$value'";
                                 }
-                            }  else {
+                            } else {
                                 $notDeletedPK[] = "'$relModel->primaryKey'";
                             }
                         }
                     }
-                    if(!$this->isNewRecord){
+                    if (!$this->isNewRecord) {
                         //DELETE WITH 'NOT IN' PK MODEL & REL MODEL
                         $notDeletedFK = implode(' AND ', $notDeletedFK);
-                        if($isCompositePK){
+                        if ($isCompositePK) {
                             $compiledNotDeletedPK = [];
-                            foreach($notDeletedPK as $attr => $pks){
-                                $compiledNotDeletedPK[$attr] = "$attr NOT IN(".implode(', ', $pks).")";
-                                if(!empty($compiledNotDeletedPK[$attr])){
-                                    $relModel->deleteAll("$notDeletedFK AND ".implode(' AND ', $compiledNotDeletedPK));
+                            foreach ($notDeletedPK as $attr => $pks) {
+                                $compiledNotDeletedPK[$attr] = "$attr NOT IN(" . implode(', ', $pks) . ")";
+                                if (!empty($compiledNotDeletedPK[$attr])) {
+                                    $relModel->deleteAll("$notDeletedFK AND " . implode(' AND ', $compiledNotDeletedPK));
                                 }
                             }
-                        }else{
+                        } else {
                             $compiledNotDeletedPK = implode(',', $notDeletedPK);
-                            if(!empty($compiledNotDeletedPK)){
-                                $relModel->deleteAll($notDeletedFK.' AND '.$relPKAttr[0]." NOT IN ($compiledNotDeletedPK)");
+                            if (!empty($compiledNotDeletedPK)) {
+                                $relModel->deleteAll($notDeletedFK . ' AND ' . $relPKAttr[0] . " NOT IN ($compiledNotDeletedPK)");
                             }
                         }
                     }
@@ -120,28 +121,102 @@ trait RelationTrait{
             throw $exc;
         }
     }
-    
-    /* this function is deprecated*/
-    public function getAttributesWithRelatedAsPost(){
+
+    public function deleteWithRelated() {
+        /* @var $this ActiveRecord */
+        $db = $this->getDb();
+        $trans = $db->beginTransaction();
+        try {
+            $error = 0;
+            $relData = $this->getRelationData();
+            foreach ($relData as $data) {
+                if($data['ismultiple']){
+                    $AQ = $this->getRelation($data['name']);
+                    $link = $AQ->link;
+                    if(count($this->{$data['name']})){
+                        $relPKAttr = $this->{$data['name']}[0]->primaryKey();
+                        $isCompositePK = (count($relPKAttr) > 1);
+                        foreach ($link as $key => $value) {
+                            if(isset($this->$value)){
+                                $array[$key] = $key . ' = ' . $this->$value;
+                            }
+                        }
+                        $error = !$this->{$data['name']}[0]->deleteAll(implode(' AND ', $array));
+                    }
+                }
+            }
+            if ($error) {
+                $trans->rollback();
+                return false;
+            }
+            if($this->delete()){
+                $trans->commit();
+                return true;
+            }
+            $trans->rollBack();
+        } catch (Exception $exc) {
+            $trans->rollBack();
+            throw $exc;
+        }
+    }
+
+    public function getRelationData() {
+        $ARMethods = get_class_methods('\yii\db\ActiveRecord');
+        $modelMethods = get_class_methods('\yii\base\Model');
+        $reflection = new \ReflectionClass($this);
+        $i = 0;
+        $stack = [];
+        /* @var $method \ReflectionMethod */
+        foreach ($reflection->getMethods() as $method) {
+            if (in_array($method->name, $ARMethods) || in_array($method->name, $modelMethods)) {
+                continue;
+            }
+            if($method->name === 'bindModels')  {continue;}
+            if($method->name === 'attachBehaviorInternal')  {continue;}
+            if($method->name === 'loadAll')  {continue;}
+            if($method->name === 'saveAll')  {continue;}
+            if($method->name === 'getRelationData')  {continue;}
+            if($method->name === 'getAttributesWithRelatedAsPost')  {continue;}
+            if($method->name === 'getAttributesWithRelated')  {continue;}
+            if($method->name === 'deleteWithRelated')  {continue;}
+            try {
+                $rel = $this->{$method->name}();
+                if($rel instanceof \yii\db\ActiveQuery){
+                    $stack[$i]['name'] = lcfirst(str_replace('get', '', $method->name));
+                    $stack[$i]['method'] = $method->name;
+                    $stack[$i]['ismultiple'] = $rel->multiple;
+                    $i++;
+                }
+            } catch (\yii\base\ErrorException $exc) {
+//                
+            }
+        }
+        return $stack;
+    }
+
+    /* this function is deprecated */
+
+    public function getAttributesWithRelatedAsPost() {
         $return = [];
         $shortName = StringHelper::basename(get_class($this));
         $return[$shortName] = $this->attributes;
-        foreach($this->relatedRecords as $records){
-            foreach($records as $index => $record){
+        foreach ($this->relatedRecords as $records) {
+            foreach ($records as $index => $record) {
                 $shortNameRel = StringHelper::basename(get_class($record));
                 $return[$shortNameRel][$index] = $record->attributes;
             }
         }
         return $return;
     }
-    
-    public function getAttributesWithRelated(){
+
+    public function getAttributesWithRelated() {
         $return = $this->attributes;
-        foreach($this->relatedRecords as $name => $records){
-            foreach($records as $index => $record){
+        foreach ($this->relatedRecords as $name => $records) {
+            foreach ($records as $index => $record) {
                 $return[$name][$index] = $record->attributes;
             }
         }
         return $return;
     }
+
 }
