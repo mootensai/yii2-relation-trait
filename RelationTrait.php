bd014cc32e7ff8d111ccdfcab14a3eaa6adae35f
Yohanes Candrajaya <moo.tensai@gmail.com>
2016-02-12 12:05:49 +0700

    fix delete related many-many
    TODO :
    fix delete related many-many when no relation left
    add feature for has one relation

--- RelationTrait.php
+++ RelationTrait.php
@@ -15,9 +15,11 @@ use \yii\db\Exception;
 use \yii\helpers\Inflector;
 use \yii\helpers\StringHelper;
 
-trait RelationTrait {
+trait RelationTrait
+{
 
-    public function loadAll($POST) {
+    public function loadAll($POST)
+    {
         if ($this->load($POST)) {
             $shortName = StringHelper::basename(get_class($this));
             foreach ($POST as $key => $value) {
@@ -31,17 +33,17 @@ trait RelationTrait {
                     $relModelClass = $rel->modelClass;
                     $relPKAttr = $relModelClass::primaryKey();
                     $isManyMany = count($relPKAttr) > 1;
-                    if($isManyMany){
-                        foreach($value as $relPost){
-                            if(!empty(array_filter($relPost))){
+                    if ($isManyMany) {
+                        foreach ($value as $relPost) {
+                            if (!empty(array_filter($relPost))) {
                                 $condition = [];
                                 $condition[$this->primaryKey()[0]] = $this->primaryKey;
-                                foreach($relPost as $relAttr => $relAttrVal){
-                                    if(in_array($relAttr, $relPKAttr))
+                                foreach ($relPost as $relAttr => $relAttrVal) {
+                                    if (in_array($relAttr, $relPKAttr))
                                         $condition[$relAttr] = $relAttrVal;
                                 }
                                 $relObj = $relModelClass::findOne($condition);
-                                if(is_null($relObj)){
+                                if (is_null($relObj)) {
                                     $relObj = new $relModelClass;
                                 }
                                 $relObj->load($relPost, '');
@@ -49,10 +51,10 @@ trait RelationTrait {
                             }
                         }
                         $this->populateRelation($relName, $container);
-                    }else if ($isHasMany) {
+                    } else if ($isHasMany) {
                         $container = [];
                         foreach ($value as $relPost) {
-                            if(!empty(array_filter($relPost))) {
+                            if (!empty(array_filter($relPost))) {
                                 /* @var $relObj ActiveRecord */
                                 $relObj = (empty($relPost[$relPKAttr[0]])) ? new $relModelClass : $relModelClass::findOne($relPost[$relPKAttr[0]]);
                                 $relObj->load($relPost, '');
@@ -73,82 +75,140 @@ trait RelationTrait {
         }
     }
 
-    public function saveAll() {
+    public function saveAll()
+    {
         /* @var $this ActiveRecord */
         $db = $this->getDb();
         $trans = $db->beginTransaction();
         try {
             if ($this->save()) {
                 $error = 0;
-                if(!empty($this->relatedRecords)){
+                if (!empty($this->relatedRecords)) {
                     foreach ($this->relatedRecords as $name => $records) {
-                        if(!empty($records)){
+                        if (!empty($records)) {
+                            $isHasMany = is_array($records);
                             $AQ = $this->getRelation($name);
                             $link = $AQ->link;
                             $notDeletedPK = [];
                             $relPKAttr = $records[0]->primaryKey();
-                            $isCompositePK = (count($relPKAttr) > 1);
-                            /* @var $relModel ActiveRecord */
-                            foreach ($records as $index => $relModel) {
-                                $notDeletedFK = [];
-                                foreach ($link as $key => $value) {
-                                    $relModel->$key = $this->$value;
-                                    $notDeletedFK[$key] = "$key = '{$this->$value}'";
-                                }
-                                $relSave = $relModel->save();
-                                if (!$relSave || !empty($relModel->errors)) {
-                                    $relModelWords = Inflector::camel2words(StringHelper::basename($AQ->modelClass));
-                                    $index++;
-                                    foreach ($relModel->errors as $validation) {
-                                        foreach ($validation as $errorMsg) {
-                                            $this->addError($name, "$relModelWords #$index : $errorMsg");
+                            $isManyMany = (count($relPKAttr) > 1);
+                            if ($isHasMany) {
+                                /* @var $relModel ActiveRecord */
+                                $i = 0;
+                                foreach ($records as $index => $relModel) {
+                                    $notDeletedFK = [];
+                                    foreach ($link as $key => $value) {
+                                        $relModel->$key = $this->$value;
+                                        if($isManyMany) $notDeletedFK[$key] = $this->$value;
+                                        elseif($isHasMany) $notDeletedFK[$key] = "$key = '{$this->$value}'";
+                                    }
+                                    $relSave = $relModel->save();
+
+                                    if (!$relSave || !empty($relModel->errors)) {
+                                        $relModelWords = Inflector::camel2words(StringHelper::basename($AQ->modelClass));
+                                        $index++;
+                                        foreach ($relModel->errors as $validation) {
+                                            foreach ($validation as $errorMsg) {
+                                                $this->addError($name, "$relModelWords #$index : $errorMsg");
+                                            }
+                                        }
+                                        $error = 1;
+                                    } else {
+                                        //GET PK OF REL MODEL
+                                        if ($isManyMany) {
+                                            foreach ($relModel->primaryKey as $attr => $value) {
+                                                $notDeletedPK[$i][$attr] = "'$value'";
+                                                $fields[$attr] = "";
+                                            }
+                                        } else {
+                                            $notDeletedPK[] = "'$relModel->primaryKey'";
                                         }
                                     }
-                                    $error = 1;
-                                } else {
-                                    //GET PK OF REL MODEL
-                                    if ($isCompositePK) {
-                                        foreach ($relModel->primaryKey as $attr => $value) {
-                                            $notDeletedPK[$attr][] = "'$value'";
+                                    $i++;
+                                }
+                                if (!$this->isNewRecord) {
+                                    //DELETE WITH 'NOT IN' PK MODEL & REL MODEL
+                                    if ($isManyMany) {
+//                                        echo "composite pk\n";
+//                                        echo "not deleted PK : \n";
+//                                        print_r($notDeletedPK);
+//                                        echo "not deleted FK : \n";
+//                                        print_r($notDeletedFK);
+//                                        echo "\nfields : \n";
+//                                        print_r($fields) . "\n";
+                                        $contoh = ['and',['actor_id' => 1],['not in', new \yii\db\Expression('(actor_id, film_id)'),
+                                            [
+                                                new \yii\db\Expression('(1,1)'),
+                                                new \yii\db\Expression('(1,23)'),
+                                                new \yii\db\Expression('(1,25)'),
+                                                new \yii\db\Expression('(1,980)'),
+                                                new \yii\db\Expression('(1,970)'),
+                                                new \yii\db\Expression('(1,939)')
+                                            ]]];
+//                                        echo $relModel::find()->where(['and',['actor_id' => 1],['not in', new \yii\db\Expression('(actor_id, film_id)'),
+//                                            [
+//                                                new \yii\db\Expression('(1,1)'),
+//                                                new \yii\db\Expression('(1,23)'),
+//                                                new \yii\db\Expression('(1,25)'),
+//                                                new \yii\db\Expression('(1,980)'),
+//                                                new \yii\db\Expression('(1,970)'),
+//                                                new \yii\db\Expression('(1,939)')
+//                                            ]]])->createCommand()->rawSql;
+                                        $compiledFields = implode(", ", array_keys($fields));
+                                        $compiledNotDeletedPK = ['and',$notDeletedFK];
+                                        $notIn = ['not in', new \yii\db\Expression("($compiledFields)")];
+                                        foreach($notDeletedPK as $value){
+                                            $v = [];
+                                            foreach($fields as $key => $f){
+                                                $v[] = $value[$key];
+                                            }
+                                            $c = implode(',',$v);
+                                            $content[] = new \yii\db\Expression("($c)");
+                                        }
+                                        array_push($notIn,$content);
+                                        array_push($compiledNotDeletedPK,$notIn);
+                                        try{
+                                            $relModel->deleteAll($compiledNotDeletedPK);
+                                        } catch (\yii\db\IntegrityException $exc) {
+                                            $this->addError($name, "Data can't be deleted because it's still used by another data.");
                                         }
                                     } else {
-                                        $notDeletedPK[] = "'$relModel->primaryKey'";
-                                    }
-                                }
-                            }
-                            if (!$this->isNewRecord) {
-                                //DELETE WITH 'NOT IN' PK MODEL & REL MODEL
-                                $notDeletedFK = implode(' AND ', $notDeletedFK);
-                                if ($isCompositePK) {
-                                    echo "composite pk\n";
-                                    $compiledNotDeletedPK = [];
-                                    foreach ($notDeletedPK as $attr => $pks) {
-                                        $compiledNotDeletedPK[$attr] = "$attr NOT IN(" . implode(', ', $pks) . ")";
-                                        if (!empty($compiledNotDeletedPK[$attr])) {
-                                            $relModel->deleteAll("$notDeletedFK AND " . implode(' AND ', $compiledNotDeletedPK));
+                                        $notDeletedFK = implode(' AND ', $notDeletedFK);
+                                        $compiledNotDeletedPK = implode(',', $notDeletedPK);
+                                        if (!empty($compiledNotDeletedPK)) {
+                                            try {
+                                                $relModel->deleteAll($notDeletedFK . ' AND ' . $relPKAttr[0] . " NOT IN ($compiledNotDeletedPK)");
+
+                                            } catch (\yii\db\IntegrityException $exc) {
+                                                $this->addError($name, "Data can't be deleted because it's still used by another data.");
+                                            }
                                         }
-                                        print_r("$notDeletedFK AND " . implode(' AND ', $compiledNotDeletedPK)."\n");
-                                    }
-                                } else {
-                                    $compiledNotDeletedPK = implode(',', $notDeletedPK);
-                                    if (!empty($compiledNotDeletedPK)) {
-                                        $relModel->deleteAll($notDeletedFK . ' AND ' . $relPKAttr[0] . " NOT IN ($compiledNotDeletedPK)");
                                     }
                                 }
+                            } else {
+                                //Has One
                             }
+
                         }
                     }
-                }else{
+                } else {
+                    //No Children left
                     if (!$this->isNewRecord) {
                         $relData = $this->getRelationData();
-                        foreach($relData as $rel){
+                        foreach ($relData as $rel) {
                             /* @var $relModel ActiveRecord */
                             $relModel = new $rel['modelClass'];
                             $condition = [];
-                            foreach($rel['link'] as $k => $v){
-                                $condition[] = $k . " = ". $this->$v;
+                            foreach ($rel['link'] as $k => $v) {
+                                if(property_exists($this, $v))
+                                    $condition[] = $k . " = " . $this->$v;
                             }
-                            $relModel->deleteAll(implode(" AND ",$condition));
+                            try {
+                                $relModel->deleteAll(implode(" AND ", $condition));
+                            } catch (\yii\db\IntegrityException $exc) {
+                                $this->addError($rel['name'], "Data can't be deleted because it's still used by another data.");
+                                $error = 1;
+                            }
                         }
                     }
                 }
@@ -168,7 +228,8 @@ trait RelationTrait {
         }
     }
 
-    public function deleteWithRelated() {
+    public function deleteWithRelated()
+    {
         /* @var $this ActiveRecord */
         $db = $this->getDb();
         $trans = $db->beginTransaction();
@@ -176,13 +237,13 @@ trait RelationTrait {
             $error = 0;
             $relData = $this->getRelationData();
             foreach ($relData as $data) {
-                if($data['ismultiple']){
+                if ($data['ismultiple']) {
                     $link = $data['link'];
-                    if(count($this->{$data['name']})){
+                    if (count($this->{$data['name']})) {
                         $relPKAttr = $this->{$data['name']}[0]->primaryKey();
                         $isCompositePK = (count($relPKAttr) > 1);
                         foreach ($link as $key => $value) {
-                            if(isset($this->$value)){
+                            if (isset($this->$value)) {
                                 $array[$key] = $key . ' = ' . $this->$value;
                             }
                         }
@@ -194,7 +255,7 @@ trait RelationTrait {
                 $trans->rollback();
                 return false;
             }
-            if($this->delete()){
+            if ($this->delete()) {
                 $trans->commit();
                 return true;
             }
@@ -206,8 +267,8 @@ trait RelationTrait {
     }
 
 
-
-    public function getRelationData() {
+    public function getRelationData()
+    {
         $ARMethods = get_class_methods('\yii\db\ActiveRecord');
         $modelMethods = get_class_methods('\yii\base\Model');
         $reflection = new \ReflectionClass($this);
@@ -218,17 +279,33 @@ trait RelationTrait {
             if (in_array($method->name, $ARMethods) || in_array($method->name, $modelMethods)) {
                 continue;
             }
-            if($method->name === 'bindModels')  {continue;}
-            if($method->name === 'attachBehaviorInternal')  {continue;}
-            if($method->name === 'loadAll')  {continue;}
-            if($method->name === 'saveAll')  {continue;}
-            if($method->name === 'getRelationData')  {continue;}
-            if($method->name === 'getAttributesWithRelatedAsPost')  {continue;}
-            if($method->name === 'getAttributesWithRelated')  {continue;}
-            if($method->name === 'deleteWithRelated')  {continue;}
+            if ($method->name === 'bindModels') {
+                continue;
+            }
+            if ($method->name === 'attachBehaviorInternal') {
+                continue;
+            }
+            if ($method->name === 'loadAll') {
+                continue;
+            }
+            if ($method->name === 'saveAll') {
+                continue;
+            }
+            if ($method->name === 'getRelationData') {
+                continue;
+            }
+            if ($method->name === 'getAttributesWithRelatedAsPost') {
+                continue;
+            }
+            if ($method->name === 'getAttributesWithRelated') {
+                continue;
+            }
+            if ($method->name === 'deleteWithRelated') {
+                continue;
+            }
             try {
-                $rel = call_user_func(array($this,$method->name));
-                if($rel instanceof \yii\db\ActiveQuery){
+                $rel = call_user_func(array($this, $method->name));
+                if ($rel instanceof \yii\db\ActiveQuery) {
                     $stack[$i]['name'] = lcfirst(str_replace('get', '', $method->name));
                     $stack[$i]['method'] = $method->name;
                     $stack[$i]['ismultiple'] = $rel->multiple;
@@ -236,14 +313,16 @@ trait RelationTrait {
                     $stack[$i]['link'] = $rel->link;
                     $i++;
                 }
-            } catch (\yii\base\ErrorException $exc) {}
+            } catch (\yii\base\ErrorException $exc) {
+            }
         }
         return $stack;
     }
 
     /* this function is deprecated */
 
-    public function getAttributesWithRelatedAsPost() {
+    public function getAttributesWithRelatedAsPost()
+    {
         $return = [];
         $shortName = StringHelper::basename(get_class($this));
         $return[$shortName] = $this->attributes;
@@ -256,7 +335,8 @@ trait RelationTrait {
         return $return;
     }
 
-    public function getAttributesWithRelated() {
+    public function getAttributesWithRelated()
+    {
         $return = $this->attributes;
         foreach ($this->relatedRecords as $name => $records) {
             foreach ($records as $index => $record) {
