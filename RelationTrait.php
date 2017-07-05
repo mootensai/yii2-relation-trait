b4f821803c4a4f4645420a5f14d34d4aab14679b
Marco Patania <marco.patania@gmail.com>
2017-07-05 11:34:25 +0200

    added model words translation for error message validation

--- RelationTrait.php
+++ RelationTrait.php
@@ -29,11 +29,11 @@ trait RelationTrait
                     /* @var $relObj ActiveRecord */
                     $isHasMany = is_array($value) && is_array(current($value));
                     $relName = ($isHasMany) ? lcfirst(Inflector::pluralize($key)) : lcfirst($key);
-                    
+
                     if (in_array($relName, $skippedRelations) || !array_key_exists($relName,$relData)){
                         continue;
                     }
-                    
+
                     $AQ = $this->getRelation($relName);
                     $relModelClass = $AQ->modelClass;
                     $relPKAttr = $relModelClass::primaryKey();
@@ -102,7 +102,7 @@ trait RelationTrait
 
                         unset($fields);
                         $fields = array();
-                        
+
                         if (!empty($records)) {
                             $AQ = $this->getRelation($name);
                             $link = $AQ->link;
@@ -122,7 +122,7 @@ trait RelationTrait
                                     $relSave = $relModel->save();
 
                                     if (!$relSave || !empty($relModel->errors)) {
-                                        $relModelWords = Inflector::camel2words(StringHelper::basename($AQ->modelClass));
+                                        $relModelWords = \Yii::t('app', Inflector::camel2words(StringHelper::basename($AQ->modelClass)));
                                         $index++;
                                         foreach ($relModel->errors as $validation) {
                                             foreach ($validation as $errorMsg) {
@@ -185,7 +185,7 @@ trait RelationTrait
                                 }
                                 $relSave = $records->save();
                                 if (!$relSave || !empty($records->errors)) {
-                                    $recordsWords = Inflector::camel2words(StringHelper::basename($AQ->modelClass));
+                                    $recordsWords = \Yii::t('app', Inflector::camel2words(StringHelper::basename($AQ->modelClass)));
                                     foreach ($records->errors as $validation) {
                                         foreach ($validation as $errorMsg) {
                                             $this->addError($name, "$recordsWords : $errorMsg");
